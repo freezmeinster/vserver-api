dfdba0cb912ba5d0a93be9870fd370088c5e75b1
freez_meinster <freez_meinster@yahoo.co.id>
2011-04-16 16:24:09 +0700

    masih mau ngerjain yang list server

--- vapi.py
+++ vapi.py
@@ -3,17 +3,31 @@ import os
 import settings
 
 class Vps:
-	def init(self,**kwargs):
-		nama = kwargs['nama']
+	def detect(self,nama):
 		#scan Vserver-Rootdir untuk mengetahui semua Vps yang ada
 		vps_list = os.listdir(settings.V_ROOTDIR)
-		
+		vps_list.remove('.pkg')
 		#membandingkan apakah nama VPS yang akan dibuat sudah ada atau belum 
 		if nama in vps_list :
 			self.on_server = True
-			self.get_conf(nama)
+			self.nama = nama
 		else :
 			self.on_server = False
 			
-	def get_conf(self,*args):
-		print args
+	def get_conf(self):
+		vdir = os.path.join(settings.V_CONFDIR,self.nama)
+		raw_mem = open(vdir+'/rlimits/rss.hard','r').read().strip()
+		self.ip = open(vdir+'/interfaces/0/ip','r').read().strip()
+		self.memory = str(int(raw_mem)/250)
+		
+class VpsServer:
+	vps_obj = []
+	
+	def fetch_all(self):
+		vps_list = os.listdir(settings.V_ROOTDIR)
+		vps_list.remove('.pkg')
+		for vps in vps_list :
+			VPS = Vps()
+			self.vps_obj.append(VPS.detect(vps))
+			
+			
